d2a0c2c263ce3d2456bdc0453706d32f1f8167ac
Chris-Wilco <christopher.wilcoski@gmail.com>
2024-03-05 16:04:45 -0800

    add functionality to add and remove books from library

--- scripts.js
+++ scripts.js
@@ -1,9 +1,12 @@
 const myLibrary = [];
 
-const libraryDisplayContainer = document.getElementById(
-    "library-display-container"
-);
+const libraryCardContainer = document.getElementById("library-card-container");
+
 const addBookButton = document.getElementById("add-book-button");
+addBookButton.addEventListener("click", (e) => {
+    createNewBookCardWrapper();
+});
+
 const removeBookButton = document.getElementById("remove-book-button");
 const viewLibraryButton = document.getElementById("view-library-button");
 
@@ -12,8 +15,87 @@ function Book(title, author) {
     this.author = author;
 }
 
+function createNewBook() {
+    const bookTitle = prompt("Please enter the new book title:", "Book Title");
+    const bookAuthor = prompt(
+        "Please enter the new book author:",
+        "Book Author"
+    );
+    const thisBook = new Book(bookTitle, bookAuthor);
+    myLibrary.push(thisBook);
+
+    return thisBook;
+}
+
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
-function createNewBookCard(book) {}
+function createNewBookCardWrapper() {
+    const newBook = createNewBook();
+    createNewBookCard(newBook);
+}
+
+function createNewBookCard(newBook) {
+    const newBookCard = document.createElement("div");
+    newBookCard.classList.add("book-card");
+    newBookCard.classList.add("content-shadow");
+
+    const newBookTitle = document.createElement("div");
+    newBookCard.classList.add("book-title");
+
+    const newBookAuthor = document.createElement("div");
+    newBookCard.classList.add("book-author");
+
+    const removeBookButton = document.createElement("div");
+    removeBookButton.classList.add("remove-book-button");
+    removeBookButton.classList.add("nav-link");
+    removeBookButton.addEventListener("click", () => {
+        console.log("yea you clicked it");
+        removeBook(newBookCard);
+    });
+    removeBookButton.textContent = "Remove";
+
+    newBookTitle.textContent = newBook.title;
+    newBookAuthor.textContent = newBook.author;
+
+    newBookCard.appendChild(newBookTitle);
+    newBookCard.appendChild(newBookAuthor);
+    newBookCard.appendChild(removeBookButton);
+    libraryCardContainer.appendChild(newBookCard);
+}
+
+function populateLibraryCards() {
+    myLibrary.forEach((e) => {
+        createNewBookCard(e);
+    });
+}
+
+function generateTempLibrary() {
+    const titleList = [
+        "The Hobbit",
+        "The Fellowship of the Ring",
+        "The Two Towers",
+        "Return of the King",
+    ];
+    const authorList = [
+        "JRR Tolkien",
+        "JRR Tolkien",
+        "JRR Tolkien",
+        "JRR Tolkien",
+    ];
+
+    for (let i = 0; i < titleList.length; i++) {
+        const newBook = new Book(titleList[i], authorList[i]);
+        myLibrary.push(newBook);
+    }
+}
+
+function removeBook(parentCard) {
+    //const parentCard = e.parentElement;
+
+    parentCard.parentElement.removeChild(parentCard);
+}
+
+generateTempLibrary();
+populateLibraryCards();
